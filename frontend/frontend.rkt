004aec909e64e0201d2df6b825d7648976449f27
EduardoLR10 <dudulr10@gmail.com>
2023-10-12 15:30:08 -0300

    Import proper default-port in frontend code

--- frontend/frontend.rkt
+++ frontend/frontend.rkt
@@ -3,6 +3,7 @@
 (require racket/tcp)
 
 (module+ client
+  (require (submod RacketowerDB/backend server))
   (define (client-socket)
     (define-values (in out) (tcp-connect "localhost" default-port))
     (write "RacketowerDB test drive" out)
