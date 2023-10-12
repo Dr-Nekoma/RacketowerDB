5ee1da80be058f9bf50e1df43c17a4e8fba02bbd
EduardoLR10 <dudulr10@gmail.com>
2023-10-12 16:28:03 -0300

    Fix typo in frontend

--- frontend/frontend.rkt
+++ frontend/frontend.rkt
@@ -7,5 +7,5 @@
   (define (client-socket)
     (define-values (in out) (tcp-connect "localhost" default-port))
     (write "RacketowerDB test drive" out)
-    (close-ouput-port out)
+    (close-output-port out)
     (display (read in))))
