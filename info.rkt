10e05a748f127ffb265a081c83f266927e2cefec
EduardoLR10 <dudulr10@gmail.com>
2023-10-12 15:13:56 -0300

    Separate build-deps and deps

--- info.rkt
+++ info.rkt
@@ -3,9 +3,9 @@
 (define deps '("racket"))
 (define build-deps '("scribble-lib"
 		     "racket-doc"
-		     "rackunit-lib"
-		     "threading-lib"
-		     "beautiful-racket"))
+		     "rackunit-lib"))
+(define deps '("threading-lib"
+	       "beautiful-racket"))
 (define scribblings '(("scribblings/RacketowerDB.scrbl" ())))
 (define pkg-desc "A simple database")
 (define version "1.0")
