c35b9c7834fb5330ebc63105a904df2dcf6a963b
Marinho <marinho.pinotti@gmail.com>
2023-11-18 20:03:01 -0300

    Reformat info.rkt

--- info.rkt
+++ info.rkt
@@ -3,22 +3,22 @@
 
 (define pkg-desc "A simple database")
 (define version "1.0")
-(define pkg-authors '(lemos magueta))
-(define license '(MIT))
+(define pkg-authors '[lemos magueta])
+(define license '[MIT])
 
 (define deps
-  '("racket"
+  '["racket"
     "threading-lib"
     "beautiful-racket"
-    "struct-update-lib"))
+    "struct-update-lib"])
 
 (define build-deps
-  '("scribble-lib"
+  '["scribble-lib"
     "racket-doc"
-    "rackunit-lib"))
+    "rackunit-lib"])
 
 (define compile-omit-files
-  '("guix.scm"))
+  '["guix.scm"])
 
 (define scribblings
-  '(("scribblings/RacketowerDB.scrbl" ())))
+  '[("scribblings/RacketowerDB.scrbl" ())])
