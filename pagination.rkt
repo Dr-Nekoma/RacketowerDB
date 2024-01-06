4b6a1af81241aa87424f88c1c99cc16f7d82fb75
EduardoLR10 <dudulr10@gmail.com>
2024-01-05 22:34:14 -0300

    Add initial structs for pagination

---
+++ pagination.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require
+ ffi/unsafe
+ (only-in RacketowerDB/util checked-guard))
+
+(struct page [instances indexes] #:transparent
+  #:guard
+  (checked-guard
+   [(instances . (vectorof bytes?))
+    (indexes . (vectorof (cons/c exact-nonnegative-integer? exact-nonnegative-integer?)))]
+    (values instances indexes)))
+
+(struct pager [pages tree amount-read] #:transparent
+  #:guard
+  (checked-guard
+   [(pages . (vectorof page?))
+    (tree .  cpointer?)
+    (amount-read . exact-nonnegative-integer?)]
+    (values pages tree amount-read)))
