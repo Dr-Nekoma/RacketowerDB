7914de100f1690a2849d07215e2c7d31e48a45a8
EduardoLR10 <dudulr10@gmail.com>
2024-01-13 01:37:50 -0300

    Add tree-test function

--- bplustree.rkt
+++ bplustree.rkt
@@ -13,8 +13,8 @@
 (define-bplustree find_and_print (_fun _pointer _int _bool -> _void))
 (define-bplustree find_and_get_value (_fun _pointer _int _bool -> _int))
 
-;; (define (tree-test)
-;;   (let* [(tree (insert false 1 10))
-;;          (tree2 (insert tree 2 11))
-;;          (tree3 (insert tree2 3 12))]
-;;     (print_leaves tree3)))
+(define (tree-test)
+  (let* [(tree (insert false 1 10))
+         (tree2 (insert tree 2 11))
+         (tree3 (insert tree2 3 12))]
+    (print_leaves tree3)))
